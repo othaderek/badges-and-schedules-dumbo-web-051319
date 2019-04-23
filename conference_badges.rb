2dc4ec46c03615de551266669fb01ea9dbab4001
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-23 20:51:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,8 +24,9 @@ def assign_rooms(name)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each do
-    |
+  puts batch_badge_creator(attendees).each do |i|
+    puts i
+  end
   puts assign_rooms(attendees)
   
 end
